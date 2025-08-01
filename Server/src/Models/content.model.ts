0106a00b8a30c5e955c02f9712d6b9ca198a5d04
Satyam00915 <satyamupadhyay0495@gmail.com>
2025-08-02 00:06:31 +0530

    content_schema

--- Server/src/Models/content.model.ts
+++ Server/src/Models/content.model.ts
@@ -2,30 +2,37 @@ import mongoose from "mongoose";
 
 const contentTypes = ["image", "video", "article", "audio"];
 
-const contentSchema = new mongoose.Schema({
-  link: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    enum: contentTypes,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  tags: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Tag",
+const contentSchema = new mongoose.Schema(
+  {
+    fileName: {
+      type: String,
     },
-  ],
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    externalUrl: {
+      type: String,
+    },
+    type: {
+      type: String,
+      enum: contentTypes,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    tags: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tag",
+      },
+    ],
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Content = mongoose.model("Content", contentSchema);
