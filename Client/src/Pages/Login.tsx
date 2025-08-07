69a3d6bac402420d927980c91463cb58be3176f2
Satyam00915 <satyamupadhyay0495@gmail.com>
2025-08-07 23:33:56 +0530

    edits

--- Client/src/Pages/Login.tsx
+++ Client/src/Pages/Login.tsx
@@ -6,7 +6,7 @@ export const Login = () => {
   const { theme } = useTheme();
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
+      <div className="flex w-full space-x-7 max-w-sm flex-col gap-6">
         <a href="/" className="flex items-center gap-1 self-center font-medium">
           <div className=" text-primary-foreground flex items-center justify-center rounded-md">
             {theme === "light" ? (
--- Client/src/Pages/SignUp.tsx
+++ Client/src/Pages/SignUp.tsx
@@ -6,9 +6,9 @@ const SignUp = () => {
   const { theme } = useTheme();
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="/" className="flex items-center gap-1 self-center font-medium">
-          <div className=" text-primary-foreground flex items-center justify-center rounded-md">
+      <div className="flex w-full space-x-7 max-w-sm flex-col gap-6">
+        <a href="/" className="flex items-center justify-center font-medium">
+          <div className=" text-primary-foreground rounded-md">
             {theme === "light" ? (
               <LightLogo size="40pt" />
             ) : (
