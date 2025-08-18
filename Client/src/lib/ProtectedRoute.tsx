8956c8ab68bb5d54128cebf78ba88011497123be
Satyam00915 <satyamupadhyay0495@gmail.com>
2025-08-18 13:31:55 +0530

    Loader

--- Client/src/lib/ProtectedRoute.tsx
+++ Client/src/lib/ProtectedRoute.tsx
@@ -1,17 +1,15 @@
- 
- 
-import Loader from "@/components/ui/Loader";
+import AdvLoader from "@/components/ui/AdvLoader";
 import { useAuthStore } from "@/store/authStore";
 import { Navigate, Outlet } from "react-router-dom";
 
 export const ProtectedRoute = () => {
   const { isLoggedIn } = useAuthStore();
-  if (isLoggedIn == null) return <Loader />;
+  if (isLoggedIn == null)
+    return (
+      <div className="flex justify-center items-center">
+        <AdvLoader />
+      </div>
+    );
   if (!isLoggedIn) return <Navigate to="/login" />;
   return <Outlet />;
 };
- 
- 
- 
-   
-   
