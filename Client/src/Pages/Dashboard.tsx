17e8777a3211c487045f4727b60a60a4a0b6e3dd
Satyam00915 <satyamupadhyay0495@gmail.com>
2025-08-21 19:19:49 +0530

    ...

--- Client/src/Pages/Dashboard.tsx
+++ Client/src/Pages/Dashboard.tsx
@@ -33,7 +33,7 @@ import {
 } from "@/components/ui/select";
 import { AudioUploadDemo, ImageUploadDemo } from "@/components/ui/ImageUpload";
 import { useEffect, useState } from "react";
-import { Tag, TagGroup } from "@/components/ui/tag-group";
+import { Tag } from "@/components/ui/tag-group";
 import api from "@/lib/api";
 import toast, { Toaster } from "react-hot-toast";
 
@@ -226,6 +226,7 @@ export const Dashboard = () => {
                     <div className="grid gap-2">
                       <div className="flex">
                         {tags.map((item) => (
+                          //@ts-expect-error Present
                           <Tag key={item._id}>{item.title}</Tag>
                         ))}
                       </div>
